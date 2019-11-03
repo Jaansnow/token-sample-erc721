9d2259b4d3dec9a8c10ef7ce0d62982ee3afbb95
Shadman <34666977+Jaansnow@users.noreply.github.com>
2019-11-03 01:33:24 -0500

    Update Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,5 +1,11 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+const MyERC721 = artifacts.require("./MyERC721.sol");
+module.exports = async function(deployer) {
+  await deployer.deploy(MyERC721, "MyERC721 Token", "MyERC721")
+  const erc721 = await MyERC721.deployed()
+}
+
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
